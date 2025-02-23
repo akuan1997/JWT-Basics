042c4af244d38ce4e92fce1095414eadb16c309a
akuan1997 <129217093+akuan1997@users.noreply.github.com>
2025-02-23 12:54:42 +0800

    auth in middleware

--- controllers/main.js
+++ controllers/main.js
@@ -13,27 +13,34 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    // console.log(req.headers)
-    const authHeader = req.headers.authorization;
-    // console.log(authHeader);
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new customAPIError('No token provided', 401)
-    }
-    const token = authHeader.split(' ')[1]
-    console.log(token);
-    console.log(process.env.JWT_SECRET);
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log('a' + decoded)
-        const luckyNumber = Math.floor(Math.random() * 100)  // 0 ~ 99
+    // // console.log(req.headers)
+    // const authHeader = req.headers.authorization;
+    // // console.log(authHeader);
+    // if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    //     throw new customAPIError('No token provided', 401)
+    // }
+    // const token = authHeader.split(' ')[1]
+    // console.log(token);
+    // console.log(process.env.JWT_SECRET);
+    // try {
+    //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //     console.log('a' + decoded)
+    //     const luckyNumber = Math.floor(Math.random() * 100)  // 0 ~ 99
+    //
+    //     res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+    //     // console.log(decoded)
+    // } catch (error) {
+    //     console.log(error)
+    //     throw new customAPIError('Not authorized to access this route', 401)
+    // }
+    console.log('2')
+    console.log(req.user)
+    const luckyNumber = Math.floor(Math.random() * 100)  // 0 ~ 99
 
-        res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
-        // console.log(decoded)
-    } catch (error) {
-        console.log(error)
-        throw new customAPIError('Not authorized to access this route', 401)
-    }
-
+    res.status(200).json({
+        msg: `Hello, ${req.user.username}`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
+    // console.log(decoded)
 }
 
 module.exports = {
---
+++ middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const customAPIError = require('../errors/custom-error')
+
+const authenticationMiddleware = async (req, res, next) => {
+    console.log('1-1')
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new customAPIError('No token provided', 401)
+    }
+    const token = authHeader.split(' ')[1]
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const { id, username } = decoded;
+        req.user = { id, username };
+        console.log('1-2')
+        next()
+    } catch (error) {
+        throw new customAPIError('Not authorized to access this route', 401)
+    }
+
+}
+
+module.exports = authenticationMiddleware
